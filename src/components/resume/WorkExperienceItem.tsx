5d0b4e47bc14e95d472e98167f415f350ba6fd94
christian <c.martinez@mytaxi.com>
2021-02-25 16:52:22 +0100

    refactor(Resume): Add `WorkExperienceItem` component into resume container

---
+++ src/components/resume/WorkExperienceItem.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+    font-size: 1.2em;
+`
+
+type Item = {
+    id: string
+    position: string
+    company: string
+    website?: string
+    startDate: string
+    endDate?: string
+    highlights: Array<string>
+    keywords: Array<string>
+}
+
+function WorkExperienceItem({ item, open }: { item: Item; open: boolean }) {
+    const [isOpen, setIsOpen] = useState(open || false)
+    return (
+        <StyledContainer>
+            <header>
+                <span className='space-left'>
+                    <span className='fa fa-caret-down'></span>
+                </span>
+                <div className='position'>{item.position}</div>
+                <div className='company'>{item.company}</div>
+                <div className='date'>
+                    <span className='startDate'>{item.startDate}</span>
+                    <span className='endDate'>
+                        {` - ${item.endDate ?? 'Current'}`}
+                    </span>
+                </div>
+            </header>
+            {item.website && (
+                <span className='website'>
+                    <a href={item.website}>{item.website}</a>
+                </span>
+            )}
+
+            <ul className='keywords'>
+                {item.keywords.map((prop) => {
+                    const key = prop.replace(/\s/g, '').toLowerCase()
+                    return <li key={key}>{prop}</li>
+                })}
+            </ul>
+
+            {isOpen ?? (
+                <div className='item display' style={{ opacity: 1 }}>
+                    <ul className='highlights'>
+                        {item.highlights.map((prop) => {
+                            const key = prop.replace(/\s/g, '').toLowerCase()
+                            return <li key={key}>{prop}</li>
+                        })}
+                    </ul>
+                </div>
+            )}
+        </StyledContainer>
+    )
+}
+
+export default WorkExperienceItem
