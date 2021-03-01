7a3cd9a0dfa7bc03d2a81ebc51014b14d23d172f
christian <c.martinez@mytaxi.com>
2021-03-01 20:36:03 +0100

    chore: Add types.js for resume

---
+++ src/components/resume/types.d.ts
@@ -0,0 +1,59 @@
+export type Location = {
+    address?: string
+    postalCode?: string
+    city: string
+    countryCode: string
+    region: string
+}
+
+export type Profile = {
+    network: string
+    username: string
+    url: string
+}
+
+export type Basics = {
+    name: string
+    label: string
+    email: string
+    phone: string
+    summary: string
+    location: Location
+    profiles: Profile[]
+}
+
+export type Education = {
+    institution: string
+    studyType: string
+    endDate: string
+    location: Location
+}
+
+export type Work = {
+    id: string
+    company: string
+    position: string
+    website?: string
+    startDate: string
+    highlights: string[]
+    keywords: string[]
+    endDate?: string
+}
+
+export type Skill = {
+    level: string
+    keywords: string[]
+}
+
+export type Language = {
+    language: string
+    fluency: string
+}
+
+export type Resume = {
+    basics: Basics
+    education: Education[]
+    work: Work[]
+    skills: Skill[]
+    languages: Language[]
+}
